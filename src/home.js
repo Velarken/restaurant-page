4b2ae35a3d07c92e1b1a91c2dcb9af73fbfbf22c
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-07-05 14:58:59 -0400

    Moved code to render main page  into home.js

---
+++ src/home.js
@@ -0,0 +1,35 @@
+import deanImg from "./dean-welcome.png";
+import welcomeBannerLeft from "./welcome-banner-1.png";
+import welcomeBannerRight from "./welcome-banner-2.png";
+import restaurantPhoto from "./restaurant.jpg";
+
+// renders content for the home page
+export function renderHomePage() {
+    // create elements
+    const welcomeDean = document.createElement("img");
+    const welcomeTextLeft = document.createElement("img");
+    const welcomeTextRight = document.createElement("img")    
+    const restuarantMain = document.createElement("img");
+    const lowerContainer = document.createElement("div");
+    // select elements
+    const content = document.getElementById('content');
+    // set DOM element to contain images
+    welcomeDean.src = deanImg
+    welcomeTextLeft.src = welcomeBannerLeft;
+    welcomeTextRight.src = welcomeBannerRight;
+    restuarantMain.src = restaurantPhoto;
+    // add classes
+    lowerContainer.classList.add("main-content");
+    // add id's
+    welcomeDean.id = 'deanWelcome';
+    welcomeTextLeft.id = 'welcomeLeft'
+    welcomeTextRight.id = 'welcomeRight'
+    restuarantMain.id = 'mainPhoto';
+    // add generated content to the DOM
+    content.append(lowerContainer);
+    lowerContainer.appendChild(restuarantMain);
+    
+    content.appendChild(welcomeTextLeft)
+    content.appendChild(welcomeDean);
+    content.appendChild(welcomeTextRight);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import {hello} from "./script.js";
 import "./styles.css";
-alert(hello)
+import {renderHomePage} from "./home.js";
+
+
+// when Home button is clicked, render
+const homeButton = document.getElementById('home');
+homeButton.addEventListener('click', renderHomePage())
+window.onload = renderHomePage();
+// when Menu button is clicked, render
+
+// when About button is clicked, render
