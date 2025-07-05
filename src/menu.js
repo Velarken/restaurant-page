97b8487d3ef613290b6c839778d4f61cdb8e9221
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-07-05 16:10:39 -0400

    Init menu page

---
+++ src/menu.js
@@ -0,0 +1,19 @@
+export function renderMenuPage() {
+    // page head
+        // menu pun + photo of annie and britta
+    // menu container
+        // menu item container 
+            // item image (left)
+            // item description (middle)
+            // item price (right)
+        // menu item container 
+            // item image (left)
+            // item description (middle)
+            // item price (right)
+        // menu item container 
+            // item image (left)
+            // item description (middle)
+            // item price (right)
+    
+    // optionally add image inspect that shows a larger photo of each meal
+}
