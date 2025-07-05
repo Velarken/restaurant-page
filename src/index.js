b69557270b57ecd4c7f51762c52f70bc90377418
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-07-05 19:23:13 -0400

    Updated to render Menu page

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ import {renderMenuPage} from "./menu.js";
 import { renderAboutPage } from "./about.js";
 import {clearDOM} from "./clear.js"
 
-const menuButton = document.getElementById('menu');
-menuButton.addEventListener('click', () => loadMenuPage())
 window.onload = renderHomePage();
-
+// Home Button
 const homeButton = document.getElementById('home');
 homeButton.addEventListener('click', () => loadHomePage())
+// Menu Button
+const menuButton = document.getElementById('menu');
+menuButton.addEventListener('click', () => loadMenuPage())
+// About Button
+const aboutButton = document.getElementById('about');
+aboutButton.addEventListener('click', () => loadAboutPage())
 
 // when Home button is clicked
 function loadHomePage() {
     clearDOM();
     renderHomePage();
+    // add file for loadNav()
+        // add nav to DOM last
 }
 // when Menu button is clicked, render
 function loadMenuPage() {
