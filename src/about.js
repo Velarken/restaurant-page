de28f57fee4cec5174c95831a74802bcfdac2796
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-07-06 15:38:37 -0400

    Added css import and photos

--- src/about.js
+++ src/about.js
@@ -1,3 +1,32 @@
+// import photos
+import happyDean from "./imgs/dean-celebrate.png"
+import map from "./imgs/map.png"
+
 export function renderAboutPage() {
-    
+    // create elements
+    const happyDeanPhoto = document.createElement('img');
+    const mapPhoto = document.createElement('img');
+    const aboutBanner = document.createElement('div');
+    const spacer = document.createElement('div');
+    const lowerContainer = document.createElement('div');
+    // selectors 
+    const pageContent = document.querySelector('.content');
+    // add ids and classes
+    pageContent.id = 'aboutContent';
+    lowerContainer.id = 'aboutLowerContainer'
+    happyDeanPhoto.id = 'happyDeanPhoto';
+    happyDeanPhoto.classList.add('center-photo');
+    spacer.classList.add('spacer');
+    // assign images
+    happyDeanPhoto.src = happyDean;
+    mapPhoto.src = map;
+    // assign text content
+
+    // add elements to dom
+    pageContent.appendChild(aboutBanner);
+    aboutBanner.appendChild(spacer);
+    aboutBanner.appendChild(happyDeanPhoto);
+    aboutBanner.appendChild(spacer);
+    pageContent.appendChild(lowerContainer);
+    lowerContainer.appendChild(mapPhoto)
 }
