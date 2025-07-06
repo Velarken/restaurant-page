84fbd1b5d0b74c78892f7a53f9ae0fd65e879381
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-07-06 15:39:05 -0400

    Cleaned up generated elements

--- src/home.js
+++ src/home.js
@@ -3,6 +3,7 @@ import deanImg from "./imgs/dean-welcome.png";
 import welcomeBannerLeft from "./imgs/welcome-banner-1.png";
 import welcomeBannerRight from "./imgs/welcome-banner-2.png";
 import restaurantPhoto from "./imgs/restaurant.jpg";
+import './home.css'
 
 // NEED TO HAVE THE RESTAURANT PHOTO BE THE BACKGROUND IMAGE OF A DIV
     // SET TESITIMONIAL WINDOW TO APPEAR IN CENTER OF THAT DIV
@@ -14,9 +15,12 @@ export function renderHomePage() {
     const welcomeDean = document.createElement("img");
     const welcomeTextLeft = document.createElement("img");
     const welcomeTextRight = document.createElement("img")    
-    const restuarantMain = document.createElement("img");
     const lowerContainer = document.createElement("div");
     const testimonial = document.createElement('div');
+    const upperContainer = document.createElement('div');
+    const leftDiv = document.createElement('div');
+    const middleDiv = document.createElement('div');
+    const rightDiv = document.createElement('div');
     // select elements
     const content = document.querySelector('.content');
     content.id = 'homeContent'
@@ -25,23 +29,30 @@ export function renderHomePage() {
     welcomeDean.src = deanImg
     welcomeTextLeft.src = welcomeBannerLeft;
     welcomeTextRight.src = welcomeBannerRight;
-    restuarantMain.src = restaurantPhoto;
     testimonial.innerHTML = "Gaaaaayyyy marrriiaaaggggeee!<br><br>-- Satan"
     // add classes
     lowerContainer.classList.add("main-content");
+    upperContainer.classList.add('welcome');
     // add id's
     welcomeDean.id = 'deanWelcome';
     welcomeTextLeft.id = 'welcomeLeft'
     welcomeTextRight.id = 'welcomeRight'
-    restuarantMain.id = 'mainPhoto';
     testimonial.id = 'testimonial'
+    leftDiv.classList.add('bannerCard');
+    leftDiv.id = 'left';
+    middleDiv.classList.add('bannerCard');
+    middleDiv.id = 'middle';
+    rightDiv.classList.add('bannerCard');
+    rightDiv.id = 'right';
     // add generated content to the DOM
-    
-    lowerContainer.appendChild(restuarantMain);
 
-    contentContainer.appendChild(welcomeTextLeft)
-    contentContainer.appendChild(welcomeDean);
-    contentContainer.appendChild(welcomeTextRight);
+    contentContainer.appendChild(upperContainer)
+    leftDiv.appendChild(welcomeTextLeft)
+    middleDiv.appendChild(welcomeDean);
+    rightDiv.appendChild(welcomeTextRight);
+    upperContainer.appendChild(leftDiv);
+    upperContainer.appendChild(middleDiv);
+    upperContainer.appendChild(rightDiv);
     contentContainer.append(lowerContainer);
     lowerContainer.appendChild(testimonial);
 }
